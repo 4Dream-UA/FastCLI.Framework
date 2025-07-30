e2aeeb4f2f9ab64092c74743d096c030318ee60d
Евгений Наволоков <goustg62@gmail.com>
2025-07-30 14:09:32 +0300

    Add docstrings with information of exception raising in wait

--- CLI/validaters.py
+++ CLI/validaters.py
@@ -2,18 +2,37 @@ class BaseValidater:
 
     @staticmethod
     def validate_not_args(args: list) -> bool:
+        """
+        This validater used for raise NoRegisteredCommandsInArguments ...
+        Exception if return True
+
+        :param args:
+        :return:
+        """
         if not args:
             return True
         return False
 
     @staticmethod
     def validate_not_func(func: callable) -> bool:
+        """
+        This validator used for raise UnexpectedCommandInArguments ...
+        Exception if return True
+        :param func:
+        :return:
+        """
         if not func:
             return True
         return False
 
     @staticmethod
     def validate_missing(missing: list) -> bool:
+        """
+        This validator used for raise CommandWithoutRegisteredMissing ...
+        Exception if return True
+        :param missing:
+        :return:
+        """
         if missing:
             raise True
         return False
