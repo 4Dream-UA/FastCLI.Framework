074cafda8c5eb05ca58583e8aff538df47b41864
Евгений Наволоков <goustg62@gmail.com>
2025-07-30 13:46:34 +0300

    Add BaseParser Class with parse_params staticmethod for pase base frameword syntax

---
+++ CLI/parsers.py
@@ -0,0 +1,18 @@
+import ast
+
+
+class BaseParser:
+    @staticmethod
+    def parse_params(args: list) -> dict:
+        if "/:" not in args or ":/" not in args:
+            return {}, args
+
+        start = args.index("/:") + 1
+        end = args.index(":/", start)
+        params = {
+            k.strip(): v.strip('"').strip("'")
+            for k, v in (arg.split("=", 1) for arg in args[start:end] if "=" in arg)
+        }
+        remaining_args = args[end + 1:]
+
+        return params, remaining_args
