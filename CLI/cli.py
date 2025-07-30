d0b8862a26dd9a37cbf4075c60c1862bad972b5e
Евгений Наволоков <goustg62@gmail.com>
2025-07-30 13:59:40 +0300

    Add command decorater to save command data

--- CLI/cli.py
+++ CLI/cli.py
@@ -15,3 +15,22 @@ class CLI:
         self.commands = {}
         self.base_parser = BaseParser()
         self.base_validator = BaseValidater()
+
+
+    def command(
+        self,
+        name: str,
+        multitypes: bool = False,
+        expose: bool = False,
+        expose_prompt: str = "Do you sure? [y/n] ",
+        expose_yes_tag: str = "--yes",
+        expose_no_tag: str = "--no",
+    ) -> callable:
+        def decorator(func):
+            self.commands[name] = {
+                "func": func, "multitypes": multitypes,
+                "expose": expose, "expose_prompt": expose_prompt,
+                "expose_yes_tag": expose_yes_tag, "expose_no_tag": expose_no_tag,
+            }
+            return func
+        return decorator
