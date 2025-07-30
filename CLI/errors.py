70c7a0879e9663c04646c267865ab6a4eda2f4cf
Евгений Наволоков <goustg62@gmail.com>
2025-07-30 13:43:19 +0300

    Add UnexpectedCommandInArguments Expception for handling alies commands in sys.args

--- CLI/errors.py
+++ CLI/errors.py
@@ -6,3 +6,12 @@ class NoRegisteredCommandsInArguments(Exception):
         If this exception is mistake - just use try-expect...
         to ignore it.
         """)
+
+
+class UnexpectedCommandInArguments(Exception):
+
+    def __init__(self, cmd) -> None:
+        super().__init__(f"""
+        You use an unexpected command!
+        Please, just delete {cmd} command and try it again.
+        """)
