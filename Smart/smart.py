757fbdbc4c5fb8f80042580c9649e67a50e103ea
Евгений Наволоков <goustg62@gmail.com>
2025-08-07 15:20:53 +0300

    Add Markdown Class parm in main TUI smart.py

--- Smart/smart.py
+++ Smart/smart.py
@@ -1,4 +1,5 @@
 from Smart.color import Color
+from Smart.markdown import Markdown
 from Smart.panel import Panel
 from Smart.emoji import Emoji
 
@@ -6,13 +7,17 @@ from Smart.emoji import Emoji
 class Smart:
 
     @property
-    def color(self):
+    def color(self) -> Color:
         return Color()
 
     @property
-    def panel(self):
+    def panel(self) -> Panel:
         return Panel()
 
     @property
-    def emoji(self):
+    def emoji(self) -> Emoji:
         return Emoji()
+
+    @property
+    def markdown(self) -> Markdown:
+        return Markdown()
